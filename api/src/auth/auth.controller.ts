940e3084b02e05641da2e9041ce1c2a5621f6133
RiznykDmytroKoitechs <115988767+RiznykDmytroKoitechs@users.noreply.github.com>
2022-11-30 14:38:13 +0200

    Basic authentification/authorisation

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+  @Post('signup')
+  SingUp(@Body() dto: AuthDTO) {
+    return this.authService.SignUp(dto);
+  }
+
+  @Post('signin')
+  Login(@Body() dto: AuthDTO) {
+    return this.authService.SingIn(dto);
+  }
 }
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
-@Injectable()
-export class AuthService {}
+@Injectable({})
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async SignUp(dto: AuthDTO) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      )
+        throw new ForbiddenException('Credentials taken');
+      throw error;
+    }
+  }
+
+  async SingIn(dto: AuthDTO) {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials Incorrect');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) throw new ForbiddenException('Credentials Incorrect');
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userid: number,
+    email: string,
+  ): Promise<{ access_string: string }> {
+    const payload = {
+      sub: userid,
+      email: email,
+    };
+    const secret = this.config.get('JWT_LOGIN_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return { access_string: token };
+  }
+}
